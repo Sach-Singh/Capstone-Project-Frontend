c988b7ff1d73320006b931329e9e53d32ebab368
Sach-singh <sach.s0501@gmail.com>
2023-05-05 17:08:22 +0530

    Dashboard

---
+++ Dashboard/app.js
@@ -0,0 +1,18 @@
+const financesTable = document.getElementById('finances').getElementsByTagName('tbody')[0];
+
+fetch('http://localhost:8080/finance/user-finances')
+  .then(response => response.json())
+  .then(data => {
+    for (let finance of data.bd) {
+      const financeRow = financesTable.insertRow();
+      financeRow.insertCell().textContent = finance.id;
+      financeRow.insertCell().textContent = finance.financeType;
+      financeRow.insertCell().textContent = finance.tag;
+      financeRow.insertCell().textContent = finance.invDt;
+      financeRow.insertCell().textContent = finance.amt;
+    //   financeRow.insertCell().textContent = finance.userDto.id;
+    //   financeRow.insertCell().textContent = finance.userDto.name;
+    //   financeRow.insertCell().textContent = finance.userDto.email;
+    }
+  })
+  .catch(error => console.error(error));

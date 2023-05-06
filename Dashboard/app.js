c19b92c72e72f8dc0e28ebca76c0f8cffe06d52c
Sach-singh <sach.s0501@gmail.com>
2023-05-06 09:36:00 +0530

    donut chart

--- Dashboard/app.js
+++ Dashboard/app.js
@@ -1,21 +1,5 @@
 const financesTable = document.getElementById('finances').getElementsByTagName('tbody')[0];
 
-// fetch('http://localhost:8080/finance/user-finances')
-//   .then(response => response.json())
-//   .then(data => {
-//     for (let finance of data.bd) {
-//       const financeRow = financesTable.insertRow();
-//       financeRow.insertCell().textContent = finance.id;
-//       financeRow.insertCell().textContent = finance.financeType;
-//       financeRow.insertCell().textContent = finance.tag;
-//       financeRow.insertCell().textContent = finance.invDt;
-//       financeRow.insertCell().textContent = finance.amt;
-//     //   financeRow.insertCell().textContent = finance.userDto.id;
-//     //   financeRow.insertCell().textContent = finance.userDto.name;
-//     //   financeRow.insertCell().textContent = finance.userDto.email;
-//     }
-//   })
-//   .catch(error => console.error(error));
 
 
   axios.get('http://localhost:8080/finance/user-finances')
@@ -34,3 +18,39 @@ const financesTable = document.getElementById('finances').getElementsByTagName('
     }
   })
   .catch(error => console.error(error));
+
+
+  const incomeEndpoint = 'http://localhost:8080/finance/total-income';
+const expensesEndpoint = 'http://localhost:8080/finance/total-expenses';
+
+// Fetch data from endpoints using Axios
+axios.all([
+  axios.get(incomeEndpoint),
+  axios.get(expensesEndpoint)
+]).then(axios.spread((incomeResponse, expensesResponse) => {
+  const income = incomeResponse.data;
+  const expenses = expensesResponse.data;
+
+  // // Calculate the total income and expenses
+  // const totalIncome = income.reduce((acc, curr) => acc + curr, 0);
+  // const totalExpenses = expenses.reduce((acc, curr) => acc + curr, 0);
+
+  // Create the donut chart using Chart.js
+  const donutChart = new Chart('donut-chart', {
+    type: 'doughnut',
+    data: {
+      labels: ['Income', 'Expenses'],
+      datasets: [{
+        data: [income, expenses],
+        backgroundColor: ['#36A2EB', '#FF6384'],
+        borderWidth: 0
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false
+    }
+  });
+})).catch(error => {
+  console.log('Error fetching data:', error);
+});
--- ForgotPassword/script.js
+++ ForgotPassword/script.js
@@ -1,27 +1,22 @@
+
 function sendResetLink(event) {
-    event.preventDefault(); // Prevent form submission
-    // var email = document.getElementById("email").value;
-    // // Validate the email address here
-    // // Generate a random token and store it in the database
-    // var token = generateToken();
-    // storeTokenInDatabase(email, token);
-    // // Send the password reset link to the user's email address
-    // var resetLink = "https://example.com/reset_password.html?email=" + encodeURIComponent(email) + "&token=" + encodeURIComponent(token);
-    // sendResetLinkToEmail(email, resetLink);
-    // Redirect the user to a confirmation page
-    alert("Reset Link Sent")
-    window.location.href = "D:/Capstone Project/Capstone-Project-Frontend/login/index.html";
-  }
-  
-//   function generateToken() {
-//     // Generate a random string of characters here
-//     return "abcdef123456";
-//   }
-  
-//   function storeTokenInDatabase(email, token) {
-//     // Store the token in the database here
-//   }
-  
-//   function sendResetLinkToEmail(email, resetLink) {
-//     // Send the reset link to the user's email address here
-//   }
+  event.preventDefault();
+
+  const email = document.getElementById('email').value;
+
+  axios.post('http://localhost:8080/users/forgot', { email })
+    .then(response => {
+      console.log(response.data);
+      if (response.data === 'SUCCESS') {
+        alert('Reset link sent successfully!');
+      window.location.href = 'D:/Capstone Project/Capstone-Project-Frontend/login/index.html';
+
+      } else if (response.data === 'FAILURE') {
+        alert('Email not registered :(');
+      }
+    })
+    .catch(error => {
+      console.error(error);
+      alert('Error sending reset link.');
+    });
+}

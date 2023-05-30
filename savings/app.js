5e83f3f93e0e6e9143011dbaa248c1890664ef40
Sach-singh <sach.s0501@gmail.com>
2023-05-30 10:49:15 +0530

    rough

--- savings/app.js
+++ savings/app.js
@@ -1,65 +1,32 @@
-// Fetch the data using Axios
-axios.get('http://localhost:8080/savings/')
-  .then(response => {
-    const savingsData = response.data.bd; // Accessing the 'bd' property containing the array of savings data
+const form = document.getElementById('savingsForm');
 
-    // Access the table body element
-    const tableBody = document.querySelector('tbody');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
 
-    // Iterate over each savings object and create table rows
-    savingsData.forEach(saving => {
-      const row = document.createElement('tr');
+  const category = document.getElementById('category').value;
+  const goal = parseFloat(document.getElementById('goal').value);
+  const currAmt = parseFloat(document.getElementById('currAmt').value);
+  const target = document.getElementById('target').value;
+  const processedDate = document.getElementById('processedDate').value;
+  const userId = parseInt(document.getElementById('userId').value);
 
-      // Create table data cells and assign values
-      const categoryCell = document.createElement('td');
-      categoryCell.textContent = saving.category;
+  const data = {
+    category: category,
+    goal: goal,
+    currAmt: currAmt,
+    target: target,
+    processedDate: processedDate,
+    userDto: {
+      id: '1'
+    }
+  };
 
-      const goalCell = document.createElement('td');
-      const goalProgressBar = createProgressBar(saving.goal, saving.currAmt);
-      goalCell.appendChild(goalProgressBar);
-
-      const currAmtCell = document.createElement('td');
-      const currAmtProgressBar = createProgressBar(saving.goal, saving.currAmt);
-      currAmtCell.appendChild(currAmtProgressBar);
-
-      const targetCell = document.createElement('td');
-      targetCell.textContent = saving.target;
-
-      const userIdCell = document.createElement('td');
-      userIdCell.textContent = saving.userDto.id;
-
-      const userNameCell = document.createElement('td');
-      userNameCell.textContent = saving.userDto.name;
-
-      const userEmailCell = document.createElement('td');
-      userEmailCell.textContent = saving.userDto.email;
-
-      // Append cells to the row
-      row.appendChild(categoryCell);
-      row.appendChild(goalCell);
-      row.appendChild(currAmtCell);
-      row.appendChild(targetCell);
-      row.appendChild(userIdCell);
-      row.appendChild(userNameCell);
-      row.appendChild(userEmailCell);
-
-      // Append the row to the table body
-      tableBody.appendChild(row);
+  axios.post('http://localhost:8080/savings/', data)
+    .then(response => {
+      console.log('Savings data posted successfully:', response.data);
+      form.reset();
+    })
+    .catch(error => {
+      console.error('Error posting savings data:', error);
     });
-  })
-  .catch(error => {
-    console.error('Error fetching data:', error);
-  });
-
-// Function to create a progress bar element
-function createProgressBar(goal, currAmt) {
-  const progressBar = document.createElement('div');
-  progressBar.className = 'progress';
-
-  const progressValue = (currAmt / goal) * 100;
-  progressBar.innerHTML = `
-    <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" style="width: ${progressValue}%" aria-valuenow="${progressValue}" aria-valuemin="0" aria-valuemax="100">${progressValue}%</div>
-  `;
-
-  return progressBar;
-}
+});
